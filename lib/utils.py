4dac3b4af5eb487df224a55c96718c1ed2e4f4e5
buciski <maurivittorio@gmail.com>
2021-03-14 11:54:48 +0100

    utils added and refactor

---
+++ lib/utils.py
@@ -0,0 +1,16 @@
+# function to be used inside command callback function
+from vat_validator.vies import check_vat
+
+
+def search_VAT(vat, country):
+
+    VAT_info = check_vat(country, vat)
+    print("all info extracted-->> " + VAT_info)
+    if VAT_info.valid:
+        return "NAME: " + str(VAT_info.name) + "\n\nADDRESS: " + str(VAT_info.address)
+    else:
+        return "Invalid response for " + VAT_info.vat + " and country " + VAT_info.country_code
+
+
+if __name__ == "__main__":
+    pass
